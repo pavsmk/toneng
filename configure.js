b51fbad5690ad82d757b04c12fdcba386a3732eb
Pavel <semchenko.pavel@gmail.com>
2024-07-25 19:32:33 +0300

    Update configure.js

--- configure.js
+++ configure.js
@@ -37,14 +37,13 @@ let githubUsername, githubRepo, botUsername;
     const params = url.match(/github.com[/:]([^/]*)\/(.*)\.git/);
     githubUsername = params[1];
     githubRepo = params[2];
-  } catch (e) {}
+  } catch (e) { }
 
   const accessToken = await question("Enter your bot access token: ");
   if (!accessToken?.length > 0) exitError("Token is required");
 
   const githubUsernameQ = await question(
-    `Enter your github username${
-      githubUsername ? ` (${githubUsername})` : ``
+    `Enter your github username${githubUsername ? ` (${githubUsername})` : ``
     }: `
   );
   githubUsername = githubUsernameQ || githubUsername;
